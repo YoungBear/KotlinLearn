ee08d90d2e1ec08b0a9c72467106ceb8919b32df
youngbear <yangshaoxiong2008@163.com>
2017-12-01 18:13:48 +0800

    temp commit for class learn
    Signed-off-by: youngbear <yangshaoxiong2008@163.com>

---
+++ src/com/ysx/hello/ClassTest.kt
@@ -0,0 +1,19 @@
+package com.ysx.hello
+
+class Runoob {
+    var name: String = "cainiao"
+    var url: String = "www.yangshaoxiong.tech"
+    var city: String = "Xi'an"
+}
+
+fun main(args: Array<String>) {
+    val site = Runoob()
+    println(site.name)
+    println(site.url)
+    println(site.city)
+}
+
+class Person {
+    var lastName: String = "zhang"
+
+}
