9a187732341099c9f067d8c9edae054d446869b2
YoungBear <yangshaoxiong2008@163.com>
2017-12-03 22:57:14 +0800

    commit for class
    Signed-off-by: YoungBear <yangshaoxiong2008@163.com>

--- src/com/ysx/hello/ClassTest.kt
+++ src/com/ysx/hello/ClassTest.kt
@@ -6,14 +6,44 @@ class Runoob {
     var city: String = "Xi'an"
 }
 
-fun main(args: Array<String>) {
-    val site = Runoob()
-    println(site.name)
-    println(site.url)
-    println(site.city)
-}
+//fun main(args: Array<String>) {
+//    val site = Runoob()
+//    println(site.name)
+//    println(site.url)
+//    println(site.city)
+//}
 
 class Person {
     var lastName: String = "zhang"
+        get() = field.toUpperCase()
+        set
+
+    var no: Int = 100
+        get() = field
+        set(value) {
+            if (value < 10) {
+                field = value
+            } else {
+                field = -1
+            }
+        }
+
+    var height: Float = 145.4F
+        private set
+
+}
+
+fun main(args: Array<String>) {
+    var person: Person = Person()
+
+    person.lastName = "wang"
+
+    println("lastName: ${person.lastName}")
+
+    person.no = 9
+    println("no:${person.no}")
+
+    person.no = 20
+    println("no:${person.no}")
 
 }
